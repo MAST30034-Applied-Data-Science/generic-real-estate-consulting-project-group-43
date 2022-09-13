fd8953fb2a72bbc386d434a5b90867034c407e6d
anzhec <anzhe.cai@student.unimelb.edu.au>
2022-09-14 06:15:32 +1000

    created add distance function but 3500 instances each time unresolved

--- scripts/add_distance.py
+++ scripts/add_distance.py
@@ -1,10 +1,85 @@
+import openrouteservice as ors
+import numpy as np
+import time
+
+"""This function accepts a property-place merged csv, and compute the distance/time travelled by car from each property to the parks/schools/stations, 
+and to Melbourne Central"""
+
+def add_distance_time(property_place_csv, year):
+    client = ors.Client('5b3ce3597851110001cf6248ce6c95ac96814219a4c3a7741f323b73') # philip's api key
+    to_place_distances = []
+    to_place_times = []
+    to_cbd_distances = []
+    to_cbd_times = []
+
+    print(f"{len(property_place_csv['SA2_CODE'].unique())} unique SA2 codes")
+    for code in property_place_csv['SA2_CODE'].unique():
+        locations_coor = []
+        sources_index = []
+        destinations_index = []
+        temp_list1 = []
+        temp_list2 = []
+
+        subset = property_place_csv[property_place_csv['SA2_CODE'] == code].reset_index(drop=True)
+        count_places = subset['Place_Names'].nunique() # e.g. 10 places for 202021027
+        i = 0
+        while i < (len(subset) // count_places): # e.g. 210 // 10 = 21 properties for 202021027
+            locations_coor.append((subset.iloc[i * count_places]['longitude_ori'], subset.iloc[i * count_places]['latitude_ori'])) # append a tuple of origin long/lat coordinates
+            sources_index.append(i) # append origin indices
+            i += 1
+        
+        locations_coor += [ (x[15], x[14]) for x in subset.iloc[:count_places].to_numpy()] # 15 is place longitude, 14 is place latitude, add all places right after property coordinates
+        locations_coor += [(144.962379, -37.810454)] # add melbourne central coordinates to the end
+
+        destinations_index += [x for x in range(i, i+count_places+1)] # destination indices right after origin indices, add uptil all places have been added 
+        
+        print("locations coordinates = ")
+        print(locations_coor)
+        print("destination index = ")
+        print(destinations_index)
+
+        time.sleep(1.5) # wait for 1.5 sec
+        # add a try/except here
+        # request the distance matrix for each property to all places under current SA2 code
+        distance_matrix = ors.distance_matrix.distance_matrix(client=client, locations=locations_coor, 
+        profile='driving-car', sources=sources_index, destinations=destinations_index, metrics=['distance', 'duration'])
+
+
+        to_place_distances += [each for sublist in distance_matrix['distances'] for each in sublist[:-1]]
+        to_place_times += [each for sublist in distance_matrix['durations'] for each in sublist[:-1]]
+        temp_list1 += [each for sublist in distance_matrix['distances'] for each in sublist[-1:]]  # get the cbd distances
+        temp_list2 += [each for sublist in distance_matrix['durations'] for each in sublist[-1:]]  # get the cbd times
+        to_cbd_distances += list(np.repeat(temp_list1, count_places))  # repeat N (place count) times for each property to keep the format
+        to_cbd_times += list(np.repeat(temp_list2, count_places))
+
+    property_place_csv['dist_to_place_M'] = to_place_distances
+    property_place_csv['dist_to_place_KM'] = property_place_csv['dist_to_place_M'] / 1000
+    property_place_csv['time_to_place_S'] = to_place_times
+    property_place_csv['time_to_place_MIN'] = property_place_csv['time_to_place_S'] / 60
+    
+    property_place_csv['dist_to_cbd_M'] = to_cbd_distances
+    property_place_csv['dist_to_cbd_KM'] = property_place_csv['dist_to_cbd_M'] / 1000
+    property_place_csv['time_to_cbd_S'] = to_cbd_times
+    property_place_csv['time_to_cbd_MIN'] = property_place_csv['dist_to_cbd_S'] / 60
+    
+    property_place_csv.to_csv(f'../../data/featured/{year}_distance_rental_place.csv')
+    return property_place_csv
+
+
+
+
+
+
+
+
+"""
 import geopy
 from geopy.geocoders import GoogleV3
 import googlemaps
 import re
 
-"""This function takes in a property csv (pandas dataframe) file, and compute distance, time for each property location to Melbourne CBD, using 
-the latitude and longitude of each property, and return the imputed csv file"""
+'''This function takes in a property csv (pandas dataframe) file, and compute distance, time for each property location to Melbourne CBD, using 
+the latitude and longitude of each property, and return the imputed csv file'''
 def add_distance_time(property_csv):
     # convert to dict for faster computation
     #property_dict = property_csv[['latitude', 'longitude', 'street_address']].to_dict('records')
@@ -49,3 +124,5 @@ def add_distance_time(property_csv):
     property_csv['walk_time_min'] = property_csv['walk_time_sec'] / 60
     
     return property_csv
+
+"""
